363b7f5379ec248112348acc66efa31fb8035f80
lanxiaobin <lanxiaobin@lizhi.com>
2021-09-01 22:48:28 +0800

    更新flutter项目试试

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page 2'),
+      home: MyHomePage(title: 'Flutter Demo Home Page 3'),
     );
   }
 }
